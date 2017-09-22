df28d5b8012629103b59480f4b33cb0738295206
foxwushui <foxwushui@163.com>
2017-09-22 14:52:15 +0800

    add

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    
+    <div v-bind:title='msg2'>
+        鼠标悬停几秒钟就可以了
+    </div>
+    <p v-if='seen'>你看到我了吗</p>
+    <ul>
+      <li v-for='todo in todos' v-bind:key='todo.id'>{{todo.text}}</li>
+    </ul>
+    <button v-on:click='reversMsg'>翻转信息</button>
   </div>
 </template>
 
@@ -10,7 +17,19 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'asdasdasda'
+      msg: 'asdasdasda',
+      msg2: '页面加载与' + new Date().toLocaleString(),
+      seen: true,
+      todos: [
+        {text: '学习javascript'},
+        {text: '学习js'},
+        {text: '学习abc'}
+      ]
+    }
+  },
+  methods: {
+    reversMsg () {
+      this.msg = this.msg.split('').reverse().join('')
     }
   }
 }
