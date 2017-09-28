083101935f5a1ab83fb0c8ca865ad0266fdd5853
foxwushui <foxwushui@163.com>
2017-09-28 09:01:20 +0800

    change

--- src/components/addContacts.vue
+++ src/components/addContacts.vue
@@ -1,11 +1,7 @@
 <template>
-<<<<<<< HEAD
   <div class="addContacts">
     添加页面
-  </div>
-=======
-  <div>添加客户</div>
->>>>>>> c37b42436e774d3a76951c98cb7b56a131c7e0c1
+    </div>
 </template>
 
 <script>
@@ -13,14 +9,11 @@ export default {
   name: 'addContacts',
   data () {
     return {}
-<<<<<<< HEAD
   },
   created () {
     this.dd.biz.navigation.setRight({
       show: false
     })
-=======
->>>>>>> c37b42436e774d3a76951c98cb7b56a131c7e0c1
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
-import Index from '@/components/Index'
-import Menu from '@/components/Menu'
-import addContacts from '@/components/addContacts'
-=======
 import Contacts from '@/components/Contacts'
 import addContacts from '@/components/addContacts'
 import Business from '@/components/Business'
 import addBusiness from '@/components/addBusiness'
 import Info from '@/components/Info'
->>>>>>> c37b42436e774d3a76951c98cb7b56a131c7e0c1
 
 Vue.use(Router)
 
@@ -24,16 +18,6 @@ export default new Router({
       component: Contacts
     },
     {
-<<<<<<< HEAD
-      path: '/Menu',
-      name: 'Menu',
-      component: Menu
-    },
-    {
-      path: '/addContacts',
-      name: 'addContacts',
-      component: addContacts
-=======
       // 添加客户页面
       path: '/addContacts',
       name: 'addContacts',
@@ -56,7 +40,6 @@ export default new Router({
       path: '/Info',
       name: 'Info',
       component: Info
->>>>>>> c37b42436e774d3a76951c98cb7b56a131c7e0c1
     }
   ]
 })
