d978f79b085d1a7db57b94f7fb844609eccf9f37
foxwushui <foxwushui@163.com>
2017-09-28 23:32:19 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <!-- 主体部分，使用uirouter -->
-    <router-view></router-view>
+    <transition name="component-fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
     <!-- 底部tabbar -->
     <Tabbar v-show="tabbarShow"></Tabbar>
   </div>
@@ -40,3 +42,12 @@ export default {
   }
 }
 </script>
+<style>
+.component-fade-enter-active, .component-fade-leave-active {
+  transition: opacity .3s ease;
+}
+.component-fade-enter, .component-fade-leave-to
+/* .component-fade-leave-active for below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -33,7 +33,6 @@ export default {
   },
   methods: {
     changeView (index, url) {
-      this.$store.state.tabbar.tabIndex = index
       // 页面跳转
       this.$router.push({path: url})
     }
